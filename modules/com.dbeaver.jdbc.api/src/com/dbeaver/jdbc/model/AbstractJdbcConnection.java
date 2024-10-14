064ce0586adaef2827994a6907ab41d107b69218
Aleksei Potsetsuev <wrouds@gmail.com>
2024-10-15 00:33:25 +0800

    dbeaver/dbeaver-devops#1525 try checkstyle 4

--- modules/com.dbeaver.jdbc.api/src/com/dbeaver/jdbc/model/AbstractJdbcConnection.java
+++ modules/com.dbeaver.jdbc.api/src/com/dbeaver/jdbc/model/AbstractJdbcConnection.java
@@ -58,7 +58,7 @@ public abstract class AbstractJdbcConnection implements Connection {
     }
 
     @Override
-    public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException { // trigger checkstyle check
+    public CallableStatement prepareCall(String sql, int resultSetType, int resultSetConcurrency, int resultSetHoldability) throws SQLException { // trigger checkstyle 
         throw new SQLFeatureNotSupportedException();
     }
 
