8f965d04476b82c3fd920b2484185d43f292fd5b
Ivan Gagarkin <gagarkin@Ivans-MacBook-Pro.local>
2025-01-09 15:17:59 +0100

    dbeaver/pro#3772 Move WS client to common module

---
+++ modules/com.dbeaver.rpc/src/com/dbeaver/rpc/ws/WsConstants.java
@@ -0,0 +1,24 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp
+ *
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of DBeaver Corp and its suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to DBeaver Corp and its suppliers
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from DBeaver Corp.
+ */
+package com.dbeaver.rpc.ws;
+
+public final class WsConstants {
+    public static final String HANDSHAKE_ERROR_HEADER = "X-Handshake-Error";
+
+    private WsConstants() {
+    }
+}
