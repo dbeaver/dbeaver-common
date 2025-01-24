4629148d061441357a58c1de48e05f7be3ecad63
Alexander Skoblikov <alexandrskoblikov@gmail.com>
2025-01-24 19:51:16 +0300

    dbeaver/pro#3977 ability to load self-signet cert from a specific folder (#69)

--- modules/org.jkiss.utils/src/org/jkiss/utils/IOUtils.java
+++ modules/org.jkiss.utils/src/org/jkiss/utils/IOUtils.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -453,5 +453,11 @@ public final class IOUtils {
 
     public static boolean isFileFromDefaultFS(@NotNull Path path) {
         return path.getFileSystem().equals(FileSystems.getDefault());
+    }
+
+    public static boolean isFolderEmpty(@NotNull Path directory) throws IOException {
+        try (DirectoryStream<Path> dirStream = Files.newDirectoryStream(directory)) {
+            return !dirStream.iterator().hasNext();
+        }
     }
 }
