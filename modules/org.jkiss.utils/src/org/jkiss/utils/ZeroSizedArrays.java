4ff0954be5cc7ecb05c5f0e62b3b550a344e274a
Nikita Akilov <26031301+n5a5@users.noreply.github.com>
2025-03-05 18:03:02 +0100

    dbeaver/pro#4299 implement sharing of Object[0] and Class[0]

---
+++ modules/org.jkiss.utils/src/org/jkiss/utils/ZeroSizedArrays.java
@@ -0,0 +1,38 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp and others
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package org.jkiss.utils;
+
+/**
+ * A utils class with arrays with lengths of zero.
+ * Since array lengths in Java are non-modifiable, it is almost always possible to share zero-length arrays,
+ * rather than repeatedly allocate new ones. Such sharing may provide useful optimizations in the program runtime or footprint.
+ */
+public final class ZeroSizedArrays {
+    /**
+     * A zero-sized array of java.lang.Object
+     */
+    public final static Object[] OF_OBJECT = new Object[0];
+    /**
+     * A zero-sized array of java.lang.Class
+     */
+    @SuppressWarnings("rawtypes")
+    public final static Class[] OF_CLASS = new Class[0];
+
+    private ZeroSizedArrays() {
+        // Utility class
+    }
+}
