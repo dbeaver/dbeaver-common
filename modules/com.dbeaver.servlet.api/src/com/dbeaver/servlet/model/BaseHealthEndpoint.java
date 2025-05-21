c863a67ec415b4f89129fce47f5df308df45646f
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2025-05-21 21:05:36 +0300

    dbeaver/pro#5636 heath endpoint

---
+++ modules/com.dbeaver.servlet.api/src/com/dbeaver/servlet/model/BaseHealthEndpoint.java
@@ -0,0 +1,68 @@
+/*
+ * DBeaver - Universal Database Manager
+ * Copyright (C) 2010-2025 DBeaver Corp
+ *
+ * All Rights Reserved.
+ *
+ * NOTICE:  All information contained herein is, and remains
+ * the property of DBeaver Corp and its suppliers, if any.
+ * The intellectual and technical concepts contained
+ * herein are proprietary to DBeaver Corp and its suppliers
+ * and may be covered by U.S. and Foreign Patents,
+ * patents in process, and are protected by trade secret or copyright law.
+ * Dissemination of this information or reproduction of this material
+ * is strictly forbidden unless prior written permission is obtained
+ * from DBeaver Corp.
+ */
+package com.dbeaver.servlet.model;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+import java.io.IOException;
+import java.io.Writer;
+import java.time.LocalDateTime;
+import java.util.LinkedHashMap;
+import java.util.Map;
+
+public class BaseHealthEndpoint extends HttpServlet {
+    private static final LocalDateTime startTime = LocalDateTime.now();
+    private static final String PARAM_EXTENDED_STATUS = "extendedStatus";
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("application/json");
+        response.setHeader("Access-Control-Allow-Origin", "*");
+        boolean extendedStatus = Boolean.parseBoolean(req.getParameter(PARAM_EXTENDED_STATUS));
+        if (extendedStatus) {
+            Map<String, String> status = new LinkedHashMap<>();
+            fillExtendedData(status);
+            try (Writer writer = response.getWriter()) {
+                writer.write("{\n");
+                int i = 0;
+                for (Map.Entry<String, String> stringStringEntry : status.entrySet()) {
+                    i++;
+                    String key = stringStringEntry.getKey();
+                    String value = stringStringEntry.getValue();
+                    writer.write("    " + key + ": " + value);
+                    if (i < status.size()) {
+                        writer.write(",");
+                    }
+                    writer.write("\n");
+                }
+                writer.write("}");
+            }
+        } else {
+            try (Writer writer = response.getWriter()) {
+                writer.write("ok");
+            }
+        }
+    }
+
+    protected void fillExtendedData(Map<String, String> extendedStatus) {
+        extendedStatus.put("status", "ok");
+        extendedStatus.put("start-time", startTime.toString());
+    }
+}
