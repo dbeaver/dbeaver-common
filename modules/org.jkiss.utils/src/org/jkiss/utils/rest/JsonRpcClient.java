dd7a3f713c9b19433ae45b596961ce0b99abfd78
Aleksandr Skoblikov <aleksandr.skoblikov@dbeaver.com>
2025-05-27 15:33:19 +0300

    dbeaver/pro#5778 custom ssl context in internal client

--- modules/org.jkiss.utils/src/org/jkiss/utils/rest/JsonRpcClient.java
+++ modules/org.jkiss.utils/src/org/jkiss/utils/rest/JsonRpcClient.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -78,7 +78,7 @@ public class JsonRpcClient extends RpcClient {
             @NotNull Gson gson,
             @NotNull String userAgent
         ) {
-            super(clientClass, uri, gson, userAgent);
+            super(clientClass, uri, gson, userAgent, null);
         }
 
         @Override
