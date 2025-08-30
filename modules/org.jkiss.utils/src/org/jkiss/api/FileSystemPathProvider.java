a20b4bf91fc263a2e3004a17a4c379d26a96a8b9
serge-rider <serge@dbeaver.com>
2025-08-30 08:14:00 +0200

    dbeaver/pro#6766 Refactor common module api

--- modules/org.jkiss.utils/src/org/jkiss/api/FileSystemPathProvider.java
+++ modules/org.jkiss.utils/src/org/jkiss/api/FileSystemPathProvider.java
@@ -1,6 +1,6 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp and others
+ * Copyright (C) 2010-2025 DBeaver Corp and others
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -22,9 +22,9 @@ import java.io.IOException;
 import java.nio.file.Path;
 
 /**
- * Provides implementation of file system Path by URI
+ * Provides implementation of file system Path by path or URI
  */
-public interface NioPathProvider {
+public interface FileSystemPathProvider {
 
     @NotNull
     Path getPathByStringOrUri(@NotNull ObjectWithContextParameters context, @NotNull String pathOrUri) throws IOException;
