bb9bde110966744451e38c5305806dcd5eb1ed0d
Viktor Kirst <viktor.kirst@dbeaver.com>
2026-05-27 13:07:04 +0400

    dbeaver/pro#7075 DPI: use new UPD driver for DPI controlle (#209)
    * dbeaver/pro#7075 Refactor RestServer API to support multiple controllers
    
    * dbeaver/pro#7075 Fix code style
    
    * dbeaver/pro#7075 Add logging
    
    * dbeaver/pro#7075 Merge devel RestServer handler factory changes
    
    * dbeaver/pro#7075 Adapt RequestHandlerFactory for multiple RestServer controllers
    
    * dbeaver/pro#7075 Bind RestServer landing page to root context
    
    * dbeaver/pro#7075 Fix ResultSet#getString contract in AbstractJdbcResultSet
    
    * dbeaver/pro#7075 Disable debug log
    
    * dbeaver/pro#7075 Remove sout
    
    ---------
    
    Co-authored-by: Serge Rider <serge@dbeaver.com>

--- modules/org.jkiss.utils/src/org/jkiss/utils/rest/RpcInvocationHandler.java
+++ modules/org.jkiss.utils/src/org/jkiss/utils/rest/RpcInvocationHandler.java
@@ -1,23 +1,24 @@
 /*
  * DBeaver - Universal Database Manager
- * Copyright (C) 2010-2024 DBeaver Corp
+ * Copyright (C) 2010-2026 DBeaver Corp and others
  *
- * All Rights Reserved.
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
  *
- * NOTICE:  All information contained herein is, and remains
- * the property of DBeaver Corp and its suppliers, if any.
- * The intellectual and technical concepts contained
- * herein are proprietary to DBeaver Corp and its suppliers
- * and may be covered by U.S. and Foreign Patents,
- * patents in process, and are protected by trade secret or copyright law.
- * Dissemination of this information or reproduction of this material
- * is strictly forbidden unless prior written permission is obtained
- * from DBeaver Corp.
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  */
 package org.jkiss.utils.rest;
 
 import com.google.gson.Gson;
 import com.google.gson.JsonElement;
+import com.google.gson.JsonParseException;
 import org.jkiss.code.NotNull;
 import org.jkiss.code.Nullable;
 import org.jkiss.utils.BeanUtils;
@@ -27,8 +28,12 @@ import java.lang.reflect.*;
 import java.net.URI;
 import java.util.LinkedHashMap;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 public abstract class RpcInvocationHandler implements InvocationHandler, RestProxy {
+
+    private static final Logger log = Logger.getLogger(RpcInvocationHandler.class.getName());
 
     @NotNull
     private final Class<?> clientClass;
@@ -117,9 +122,11 @@ public abstract class RpcInvocationHandler implements InvocationHandler, RestPro
             try {
                 return gson.fromJson(contents, returnType);
             } catch (Throwable e) {
+                log.log(Level.WARNING, "Failed to parse json response: \n" + contents, e);
                 //just debug breakpoint, rethrow it
                 throw e;
             }
+
         } catch (RpcException e) {
             if (e.getErrorClass() != null) {
                 Throwable error = null;
